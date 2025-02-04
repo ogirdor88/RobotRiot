c3d57f2af522c2dbc48ad0e97e070032730f7008
Rodrigo <rlopezpatino@gmail.com>
2025-02-04 13:26:03 -0800

    testing cinemachine

--- Robot Riot/Assets/_Scripts/CameraMove.cs
+++ Robot Riot/Assets/_Scripts/CameraMove.cs
@@ -5,7 +5,7 @@ using static UnityEngine.GraphicsBuffer;
 
 public class CameraMove : MonoBehaviour
 {
-    public GameObject player;
+    /*public GameObject player;
     private Vector3 offset;
 
 
@@ -21,5 +21,33 @@ public class CameraMove : MonoBehaviour
     {
         //move the camera to follow the player with the offset
         transform.position = player.transform.position - offset;
+    }*/
+
+    public Transform orient;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody body;
+
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        //turn the cursor off
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Update()
+    {
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orient.forward = viewDir.normalized;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 inputDir = orient.forward * vertical + orient.right * horizontal;
+
+        if(inputDir != Vector3.zero)
+        {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed); 
+        }
     }
 }
