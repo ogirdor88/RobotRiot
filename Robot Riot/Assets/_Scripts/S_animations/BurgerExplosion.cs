5cf28a8fd730d9a615c4604a0221068625c39cda
nkriger <116672984+nkriger@users.noreply.github.com>
2025-03-24 20:06:50 -0700

    Mine Working with animations

--- Robot Riot/Assets/_Scripts/S_animations/BurgerExplosion.cs
+++ Robot Riot/Assets/_Scripts/S_animations/BurgerExplosion.cs
@@ -13,21 +13,26 @@ public class BurgerExplosion : MonoBehaviour
     {
         explode();
     }
+
     void explode()
     {
-
         var surroundingObjects = Physics.OverlapSphere(transform.position, _explosionRadius);
-        var rbs = GetComponentInChildren<Rigidbody>();
-        
-        Destroy(areaColldier);
-        foreach(var obj in surroundingObjects)
-        {
-            var rb = obj.GetComponent<Rigidbody>();
-            if (rb != null) continue;
 
-            rb.AddExplosionForce(_explosionForce, this.transform.position, _explosionRadius);
-            Debug.Log(surroundingObjects);
+        // Destroy the collider of the current object if applicable
+        Destroy(areaColldier);
+
+        foreach (var obj in surroundingObjects)
+        {
+            // Get the Rigidbody component of the object
+            var rb = obj.GetComponent<Rigidbody>();
+
+            // Check if the object has a Rigidbody
+            if (rb != null)
+            {
+                // Apply explosion force to the Rigidbody
+                rb.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+            }
         }
-        
+
     }
 }
