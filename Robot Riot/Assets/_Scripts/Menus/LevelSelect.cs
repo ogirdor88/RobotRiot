204750a650203b246043eedc24c41741c4bf6b03
Rodrigo <rlopezpatino@gmail.com>
2025-03-26 19:29:29 -0700

    updated levels select
    the switching to the buttons changes the current level selection to a different color so that the player knows which button they are actively on

--- Robot Riot/Assets/_Scripts/Menus/LevelSelect.cs
+++ Robot Riot/Assets/_Scripts/Menus/LevelSelect.cs
@@ -8,7 +8,7 @@ using UnityEngine.SceneManagement;
 public class LevelSelect : MonoBehaviour
 {
     [SerializeField]
-    private GameObject select, buttonSelect;
+    private GameObject select, buttonSelect, lsUnfocused;
 
     private int level, button;
 
@@ -21,32 +21,40 @@ public class LevelSelect : MonoBehaviour
         button = 2;
         select.SetActive(true);
         buttonSelect.SetActive(false);
-        MoveSelector();
+        lsUnfocused.SetActive(false);
     }
 
     private void MoveSelector()
     {
+        //if the selection is on the levels check the levels 
         if (onlevel) 
         {
+            //switch the location of the selector based on the level number
             switch (level)
             {
                 case 1:
                     select.transform.position = new Vector3(79, 567, 0);
+                    lsUnfocused.transform.position = new Vector3(79, 567, 0);
                     break;
                 case 2:
                     select.transform.position = new Vector3(568, 567, 0);
+                    lsUnfocused.transform.position = new Vector3(568, 567, 0);
                     break;
                 case 3:
                     select.transform.position = new Vector3(1057, 567, 0);
+                    lsUnfocused.transform.position = new Vector3(1057, 567, 0);
                     break;
                 case 4:
                     select.transform.position = new Vector3(1544, 567, 0);
+                    lsUnfocused.transform.position = new Vector3(1544, 567, 0);
                     break;
             }
         }
         
+        //if the selection is on the buttons check the buttons 
         if (!onlevel)
         {
+            //move the location of the selector based on the button number
             switch(button)
             {
                 case 1:
@@ -59,11 +67,17 @@ public class LevelSelect : MonoBehaviour
         }
         
     }
+    
+    //move the selector to the right
     public void SelectorRight(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            Debug.Log(" LEVEL " + level + " And Button " + button);
+            //Debug.Log(" LEVEL " + level + " And Button " + button);
+            //if the selector is on the levels
+            //check to see if it is not at the last level
+            //if not at the last level move the selecter over one 
+            //if it gets to the last level keep it at the 4th position
             if (onlevel)
             {
                 if (level < 4)
@@ -72,6 +86,10 @@ public class LevelSelect : MonoBehaviour
                     level = 4;
             }
 
+            //if the selector is on the buttons
+            //check to see if it is not at the right
+            //if not at the right button move the selecter over
+            //if its already at the right button keep it there
             if (!onlevel)
             {
                 if (button < 2)
@@ -80,6 +98,7 @@ public class LevelSelect : MonoBehaviour
                     button = 2;
             }
 
+            //update the positions after the numbers have changed
             MoveSelector();
         }
     }
@@ -88,7 +107,11 @@ public class LevelSelect : MonoBehaviour
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            Debug.Log(" LEVEL " + level + " And Button " + button);
+            //Debug.Log(" LEVEL " + level + " And Button " + button);
+            //if the selector is on the levels
+            //check to see if it is not at the first level
+            //if not at the first level move the selecter back one  
+            //if it gets to the first level keep it at the 1st position
             if (onlevel)
             {
                 if (level > 1)
@@ -97,6 +120,10 @@ public class LevelSelect : MonoBehaviour
                     level = 1;
             }
 
+            //if the selector is on the buttons
+            //check to see if it is not at the left
+            //if not at the left button move the selecter back
+            //if its already at the left button keep it there
             if (!onlevel)
             {
                 if (button > 1)
@@ -109,21 +136,34 @@ public class LevelSelect : MonoBehaviour
         }
     }
 
+    //switching to the level select
     public void SelectLevels(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
+            //make the level selecting bool true
+            //turn on the active level select visuals
+            //turn off the unfocused selection visuals
+            //turn off the button selecting visual
             onlevel = true;
             select.SetActive(true);
+            lsUnfocused.SetActive(false);
             buttonSelect.SetActive(false);
         }
     }
 
+    //switchingto button selection
     public void SelectButtons(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
+            //make the level selecting bool false
+            //turn off the active level select visuals
+            //turn on the unfocused selection visuals
+            //turn on the button selecting visual
             onlevel = false;
+            select.SetActive(false);
+            lsUnfocused.SetActive(true);
             buttonSelect.SetActive(true);
         }
     }
@@ -132,6 +172,8 @@ public class LevelSelect : MonoBehaviour
     {
         if (context.phase == InputActionPhase.Performed)
         {
+            //if the selector is on the next button 
+            //when you press a check the level that is selected then change the scene
             if (!onlevel && button == 2)
             {
                 switch (level)
@@ -150,6 +192,8 @@ public class LevelSelect : MonoBehaviour
                         break;
                 }
             }
+
+            //if the selector is on the back button change the scene to the main menu
             if (!onlevel && button == 1)
             {
                 SceneManager.LoadScene(0);
