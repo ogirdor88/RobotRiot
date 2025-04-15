0ce37591281aa692755b462bbb890f140c36c407
rpursell <148917102+rpursell@users.noreply.github.com>
2025-04-15 15:15:41 -0700

    Powerup UI
    Adds a power up UI so you can see what power up you have

---
+++ Robot Riot/Assets/_Scripts/Inventory/PowerUpUI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerUpUI : MonoBehaviour
+{
+    [SerializeField] private RawImage powerUpIcon;
+    [SerializeField] private TMP_Text powerUpName;
+    [SerializeField] private TMP_Text powerUpTimeRemaining;
+
+    private void Start()
+    {
+        UpdatePowerup(null, null, -1);
+    }
+    public void UpdatePowerup(Texture2D powerupImage, string name, int time)
+    {
+
+        // Assign textures
+        if (powerupImage != null)
+        {
+            powerUpIcon.GetComponent<RawImage>().texture = powerupImage;
+            powerUpIcon.color = new Color(powerUpIcon.color.r, powerUpIcon.color.g, powerUpIcon.color.b, 100);
+        }
+        else
+            powerUpIcon.color = new Color(powerUpIcon.color.r, powerUpIcon.color.g, powerUpIcon.color.b, 0);
+        if (powerUpName != null && name != null)
+        {
+            //weaponName.text = Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(currentWeapon.GetComponent<Weapon>().weapon));
+            powerUpName.text = name;
+        }
+        else
+            powerUpName.text = string.Empty;
+        if (powerUpTimeRemaining != null && time >= 0)
+        {
+            powerUpTimeRemaining.text = "TIME: " + time.ToString();
+        }
+        else
+            powerUpTimeRemaining.text = string.Empty;
+    }
+}
