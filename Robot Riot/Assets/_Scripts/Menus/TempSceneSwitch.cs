9a1b16fe6e25dba48bbe4966f980822ff4694564
Rodrigo <rlopezpatino@gmail.com>
2025-04-17 01:01:29 -0700

    Controller UI done

--- Robot Riot/Assets/_Scripts/Menus/TempSceneSwitch.cs
+++ Robot Riot/Assets/_Scripts/Menus/TempSceneSwitch.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Accessibility;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TempSceneSwitch : MonoBehaviour
 {
     [SerializeField] private TMP_Text player1Text;
     [SerializeField] private TMP_Text player2Text;
+
+    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject items;
+    [SerializeField] private Button creditsX, itemsX, creditsButton, itemsButton;
 
     private void Awake()
     {
@@ -32,6 +38,8 @@ public class TempSceneSwitch : MonoBehaviour
             player1Text.text = "Nobody wins";
             player2Text.text = "Nobody wins";
         }
+        //credits.SetActive(false);
+        //items.SetActive(false);
     }
 
     public void LoadNextScene()
@@ -51,5 +59,30 @@ public class TempSceneSwitch : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        credits.SetActive(true);
+        creditsX.Select();
+        Debug.Log("Credits");
+    }
+
+    public void ShowItems()
+    {
+        items.SetActive(true);
+        itemsX.Select();
+        Debug.Log("Items");
+    }
+
+    public void BackOutItems()
+    {
+        items.SetActive(false);
+        itemsButton.Select();
+    }
+    public void BackOutCredits()
+    {
+        credits.SetActive(false);
+        creditsButton.Select();
     }
 }
