410f3be3d52400f11a9440c456f8181df2301e78
rpursell <148917102+rpursell@users.noreply.github.com>
2025-04-17 15:22:54 -0700

    Bot Mode UI
    Adds a UI to let you know if you're in bot mode or not.

--- Robot Riot/Assets/_Scripts/Inventory/WeaponUI.cs
+++ Robot Riot/Assets/_Scripts/Inventory/WeaponUI.cs
@@ -17,6 +17,7 @@ public class WeaponUI : MonoBehaviour
     [SerializeField] private TMP_Text weaponName;
     [SerializeField] private TMP_Text weaponDesc;
     [SerializeField] private TMP_Text weaponAmmo;
+    [SerializeField] private TMP_Text botMode;
 
     public void UpdateWeapon()
     {
@@ -43,6 +44,13 @@ public class WeaponUI : MonoBehaviour
         nextWeaponIcon2.GetComponent<RawImage>().texture = null;
         prevWeaponIcon1.GetComponent<RawImage>().texture = null;
         prevWeaponIcon2.GetComponent<RawImage>().texture = null;
+
+        /*if (transform.parent.parent.GetComponent<PlayerController>().botMode)
+            botMode.text = "YOU ARE IN BOT MODE";
+        else
+            botMode.text = "YOU ARE NOT IN BOT MODE";*/
+
+        botMode.text = "BOT MODE: " + transform.parent.parent.GetComponent<PlayerController>().botMode.ToString().ToUpper();
 
         // Assign textures
         if (currentWeapon != null)
