f0ff94fde5512591a84e75a9f3c5cdf008931827
jpmeddings <jpmeddings@mail.csuchico.edu>
2025-04-21 21:14:49 -0700

    Update Mine.cs

--- Robot Riot/Assets/_Scripts/DamageSystem/Traps/Mine.cs
+++ Robot Riot/Assets/_Scripts/DamageSystem/Traps/Mine.cs
@@ -19,6 +19,14 @@ public class Mine : MonoBehaviour
         StartCoroutine(WaitForCooldown());
     }
 
+    private void Start()
+    {
+        if (MatchTimer.suddenDeath)
+        {
+            Destroy(gameObject, 7f);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" && canDeploy)
