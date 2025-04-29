f38d745960b4723a27ab6ec2bd0a7f428d5f2f4e
Rodrigo <rlopezpatino@gmail.com>
2025-04-29 15:53:46 -0700

    Revert "Merge branch 'Dev' into Rodrigo"
    This reverts commit 8c0e269be30ea3701b5affffb9ac0186d598f9a0, reversing
    changes made to 8ee1e0c3440347cd091bafac9717038c431d3330.

--- Robot Riot/Assets/_Scripts/DamageSystem/Weapons/BoomBox.cs
+++ Robot Riot/Assets/_Scripts/DamageSystem/Weapons/BoomBox.cs
@@ -64,8 +64,6 @@ public class BoomBox : Weapon
     private void FireWeapon()
     {
         playerMove.animator.Play("L3 BoomBox");
-        if (!GetComponent<AudioSource>().isPlaying)
-            GetComponent<AudioSource>().Play();
         damageCollider.enabled = true;
         if (!currentProjectile)
         {
@@ -88,15 +86,9 @@ public class BoomBox : Weapon
     private void StopFiring()
     {
         Destroy(currentProjectile);
-        GetComponent<AudioSource>().Stop();
         damageCollider.enabled = false;
         if (remainingAmmo == 0)
             Destroy(this.gameObject);
-    }
-
-    private void OnDisable()
-    {
-        StopFiring();
     }
 
     /*private void OnTriggerEnter(Collider other)
--- Robot Riot/Assets/_Scripts/Inventory/WeaponUI.cs
+++ Robot Riot/Assets/_Scripts/Inventory/WeaponUI.cs
@@ -17,7 +17,6 @@ public class WeaponUI : MonoBehaviour
     [SerializeField] private TMP_Text weaponName;
     [SerializeField] private TMP_Text weaponDesc;
     [SerializeField] private TMP_Text weaponAmmo;
-    [SerializeField] private TMP_Text ammoWarning;
 
     public void UpdateWeapon()
     {
@@ -113,35 +112,7 @@ public class WeaponUI : MonoBehaviour
         }
         if (weaponAmmo != null)
         {
-            if (currentWeapon.GetComponent<Weapon>().weapon.ammo != 0)
-            {
-                weaponAmmo.text = "AMMO: " + currentWeapon.GetComponent<Weapon>().remainingAmmo.ToString();
-                weaponAmmo.gameObject.SetActive(true);
-            }
-            else
-                weaponAmmo.gameObject.SetActive(false);
-        }
-        if (ammoWarning != null)
-        {
-            if (currentWeapon.GetComponent<Weapon>().weapon.ammo != 0)
-            {
-                float maxAmmo = (float)currentWeapon.GetComponent<Weapon>().weapon.ammo;
-                Debug.Log("This weapons max ammo is " + maxAmmo);
-                if (currentWeapon.GetComponent<Weapon>().remainingAmmo > 0 && (float)currentWeapon.GetComponent<Weapon>().remainingAmmo < maxAmmo / 2)
-                {
-                    ammoWarning.text = "AMMO WARNING";
-                    ammoWarning.gameObject.SetActive(true);
-                }
-                else if (currentWeapon.GetComponent<Weapon>().remainingAmmo == 0)
-                {
-                    ammoWarning.text = "OUT OF AMMO";
-                    ammoWarning.gameObject.SetActive(true);
-                }
-                else
-                    ammoWarning.gameObject.SetActive(false);
-            }
-            else
-                ammoWarning.gameObject.SetActive(false);
+            weaponAmmo.text = "AMMO: " + currentWeapon.GetComponent<Weapon>().remainingAmmo.ToString();
         }
     }
 }
