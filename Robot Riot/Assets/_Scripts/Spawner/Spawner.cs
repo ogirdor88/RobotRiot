fea0a768bc6975e7fa663ffbdb74cd384cdd1f04
Rodrigo <rlopezpatino@gmail.com>
2025-05-01 15:50:02 -0700

    spawnerWIP

--- Robot Riot/Assets/_Scripts/Spawner/Spawner.cs
+++ Robot Riot/Assets/_Scripts/Spawner/Spawner.cs
@@ -7,12 +7,13 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private List<GameObject> items;
     [SerializeField]
-    private bool random;
+    private bool random, pickup;
     [SerializeField]
     private int itemNumber;
     [SerializeField]
     private float spawndelay;
 
+    private int slotNum;
     private bool spawned;
     //private GameObject spawnedItem;
 
@@ -37,7 +38,9 @@ public class Spawner : MonoBehaviour
         int rand = Random.Range(0, items.Count);
 
         //spawn the random item
-       //spawnedItem = Instantiate(items[rand], new Vector3(transform.position.x,transform.position.y + .5f, transform.position.z), Quaternion.identity);
+        //spawnedItem = Instantiate(items[rand], new Vector3(transform.position.x,transform.position.y + .5f, transform.position.z), Quaternion.identity);
+        slotNum = items[rand].GetComponent<DeployTrapWeapon>().slot;
+        Debug.Log(items[rand].name + " Slot:" + slotNum);
        Instantiate(items[rand], new Vector3(transform.position.x,transform.position.y + .5f, transform.position.z), Quaternion.identity);
         //set the spawned bool to true
        spawned = true;
@@ -46,11 +49,18 @@ public class Spawner : MonoBehaviour
     private void ConstantSpawn(int index)
     {
         //spawnedItem = Instantiate(items[index], new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z), Quaternion.identity);
+        slotNum = items[index].GetComponent<DeployTrapWeapon>().slot;
+        Debug.Log(items[index].name + " Slot:" + slotNum);
         Instantiate(items[index], new Vector3(transform.position.x, transform.position.y + .5f, transform.position.z), Quaternion.identity);
         //set the spawned bool to true
         spawned = true;
     }
 
+    private void FindSlot(//pass a game object here)
+        )
+    {
+        //check for slot number here 
+    }
     private IEnumerator NewSpawnDelay()
     {
         yield return new WaitForSeconds(spawndelay);
@@ -63,6 +73,14 @@ public class Spawner : MonoBehaviour
         {
             //Destroy(spawnedItem);
             StartCoroutine(NewSpawnDelay());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+             
         }
     }
 }
