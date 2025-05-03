be0a60a7114a7d888ea89945bd94b59aaf3dc810
jpmeddings <jpmeddings@mail.csuchico.edu>
2025-05-03 14:52:44 -0700

    update to fix

--- Robot Riot/Assets/_Scripts/MatchTimer.cs
+++ Robot Riot/Assets/_Scripts/MatchTimer.cs
@@ -101,7 +101,7 @@ public class MatchTimer : MonoBehaviour
         yield return new WaitForSeconds(clipLength);
         camOn = true;
         stop = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
         sdTexture.SetActive(false);
         //Debug.LogWarning("CLIP PLAY SECOND");
         //Destroy(sdClip.gameObject);
